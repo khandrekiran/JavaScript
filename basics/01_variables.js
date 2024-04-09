b0dca3da391d18a1813a8d17f7e07b43b3b8d79f
Kiran Khandre <kiran.khandre@gmail.com>
2024-04-09 18:46:47 +0530

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12312312;
+let emailId = `kiran@yahoo.com`;
+var accountPassword = `123456`;
+accountCity = `Pune`;
+
+
+// accountId = 123;         // not allowed as accountId is already set to constant
+console.log(accountId);
+
+console.table([accountId, emailId, accountPassword, accountCity]);
+
+

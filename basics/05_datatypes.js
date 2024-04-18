7f242c69ec0b931e52b79e9e25023949bbc53760
Kiran Khandre <kiran.khandre@gmail.com>
2024-04-18 11:49:23 +0000

    datatypes

---
+++ basics/05_datatypes.js
@@ -0,0 +1,11 @@
+/* The old JS and the current JS have few differences. The previous JS
+didnt had classes, modules, data functions etc but the newer ones now have.
+The code written in the past and the code written currently had differences.
+
+Hence we write "use strict;" => means treat all JS code as newer version */
+
+
+//alert ("hello");
+//alert (3+3); // now we are using nodejs and not browser
+
+
